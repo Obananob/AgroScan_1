e572c8cb9b744ad630514f606a30bcf431502259
Obananob <97780446+Obananob@users.noreply.github.com>
2025-10-26 10:41:32 +0100

    Use environment variable for server port

--- main.py
+++ main.py
@@ -162,4 +162,6 @@ def root():
     return {"message": "🌱 Welcome to AgroScan FastAPI backend!"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
